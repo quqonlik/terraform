75ef52dbd256d0a24eec7a6fe7de07cb35f98bdd
quqonlik <quqonlik@gmail.com>
2022-07-15 12:27:29 -0500

    My

--- terraform-master/Start With Terraform Basics/main.tf
+++ terraform-master/Start With Terraform Basics/main.tf
@@ -2,7 +2,7 @@
 
 # Configure the AWS Provider
 provider "aws" {
-    version = "3.53.0"
+    version = "4.20.0"
     access_key = "AKIAXC3FBB2OJJC3DUIP"
     secret_key = "SECRET_KEY"
     region     = "us-east-2"
@@ -10,6 +10,6 @@ provider "aws" {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-    version = "2.72.0"
+    version = "3.11.0"
     features {}
 }
